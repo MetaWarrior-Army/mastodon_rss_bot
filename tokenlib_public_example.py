57cbae66ff64fc5b0f2db84c9ea2eacf10692708
mpoletiek <matthew.poletiek@gmail.com>
2024-03-12 07:49:34 +0000

    tokenlib

---
+++ tokenlib_public_example.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def whattokenswehave():
+	tokenlist=["example.com"]
+	return tokenlist
+
+def getmytokenfor(Mastodon_server):
+    if Mastodon_server == "example.com":
+        tokendict={
+        "host_instance": 'https://example.com',
+        "pa_token": "---",  ## create tokens yourselfin your mastodon profile
+        "botname": "enewsbot"}
+    return tokendict
+
+def getRedisVariables():
+     redis_variables = {'password':'---',
+                        'host':'localhost',
+                        'port':6379}
+     return redis_variables
+
+def getCSVUrl():
+    csv_url="https://raw.githubusercontent.com/example-com/ea_rss_bot_feeds/main/enews_rss.csv"
+    return csv_url
